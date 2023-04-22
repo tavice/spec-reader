c3b509ffb78750a4576ee6cc49648c38932166cc
tavice <thomasavice.ta@gmail.comm>
2023-04-22 14:33:15 -0700

    updated versions

--- spec-reader/src/components/FileImport.js
+++ spec-reader/src/components/FileImport.js
@@ -3,6 +3,8 @@ import { saveAs } from 'file-saver';
 import pdfjsLib from 'pdfjs-dist';
 
 const FileImport = () => {
+
+    //Function to handle file change
   const handleFileChange = (event) => {
     const file = event.target.files[0];
 
@@ -15,31 +17,36 @@ const FileImport = () => {
         parsePdf(pdfData);
       };
     }
+
+    console.log(file);
   };
 
-  const parsePdf = (pdfData) => {
-    pdfjsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.4.456/pdf.worker.min.js';
+  //Function to parse pdf
+  const pdfjs = require('pdfjs-dist/build/pdf');
 
-    pdfjsLib.getDocument({ data: pdfData }).promise.then((pdf) => {
-      const pages = [];
-      for (let i = 1; i <= pdf.numPages; i++) {
-        pdf.getPage(i).then((page) => {
-          page.getTextContent().then((textContent) => {
-            const textItems = textContent.items.map((item) => item.str);
-            const pageText = textItems.join(' ');
-            pages.push(pageText);
-
-            if (pages.length === pdf.numPages) {
-              const text = pages.join(' ');
-              const jsonData = { text };
-              downloadJson(jsonData);
-            }
-          });
-        });
+  const parsePdf = async (pdfData) => {
+    pdfjs.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.4.456/pdf.worker.min.js';
+    const pdf = await pdfjs.getDocument({ data: pdfData }).promise;
+    const pages = [];
+    for (let i = 1; i <= pdf.numPages; i++) {
+      const page = await pdf.getPage(i);
+      const textContent = await page.getTextContent();
+      const textItems = textContent.items.map((item) => item.str);
+      const pageText = textItems.join(' ');
+      pages.push(pageText);
+      if (pages.length === pdf.numPages) {
+        const text = pages.join(' ');
+        const jsonData = { text };
+        downloadJson(jsonData);
       }
-    });
-  };
+    }
 
+    console.log(pages);
+    console.log(pdf);
+  };
+  
+ 
+    //Function to download json
   const downloadJson = (jsonData) => {
     const blob = new Blob([JSON.stringify(jsonData, null, 2)], { type: 'application/json' });
     saveAs(blob, 'pdfText.json');
