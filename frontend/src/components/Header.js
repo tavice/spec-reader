5546711e3e2cb3fc38ad447e4c6ecceb824c09ac
tavice <thomasavice.ta@gmail.comm>
2023-04-24 11:23:22 -0700

    added styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css'
 
 const Header = () => {
   return (
-    <div>
-        <h1 className='mainTitle'>My Project Management Tools</h1>
-        <div className='navBar'>
-            <Link to='/'>Home</Link>
-            <Link to='/keywords'>Documents Keywords Analyzer</Link>
-            <Link to='/pmgpt'>PM GPT</Link>
-   
-            </div>
+    <div className='header'>
+      <h1 className='main-title'>My Project Management Tools</h1>
+      <nav className='nav-bar'>
+        <Link to='/' className='nav-link'>Home</Link>
+        <Link to='/keywords' className='nav-link'>Documents Keywords Analyzer</Link>
+        <Link to='/pmgpt' className='nav-link'>PM GPT</Link>
+      </nav>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
